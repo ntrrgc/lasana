3bddc8bc609af3b480c1d01cd4f4d3fdb668d7ef
Juan Luis Boya García <ntrrgc@gmail.com>
2013-03-16 00:21:23 +0100

    Added new expiration option: 6 hours. New default.

--- forms.py
+++ forms.py
@@ -10,6 +10,7 @@ class MealCreateForm(Form):
         (30, _("30 minutes")),
         (60, _("1 hour")),
         (180, _("3 hours")),
+        (360, _("6 hours")),
         (60*24, _("1 day")),
         (60*24*7, _("1 week")),
-    ), initial=180, label=_("Expires in"))
+    ), initial=360, label=_("Expires in"))
