dd902946fb0d85f31a9da5836b5c556cd3053ad2
Juan Luis Boya García <ntrrgc@gmail.com>
2013-03-16 02:28:59 +0100

    Variable stylesheets

--- context_processors.py
+++ context_processors.py
@@ -1,4 +1,19 @@
-from . import settings
+from django.conf import settings
+from . settings import LASANA_NAME
+import re
+
+basic_name = re.compile(r'^[a-zA-Z0-9_]+$')
 
 def common(request):
-    return {'lasana_name': settings.LASANA_NAME}
+    style = request.GET.get('style')
+    if style is not None:
+        # Only allow style names with letters, numbers and underscore
+        if not basic_name.match(style):
+            style = None
+
+    stylesheet = settings.STATIC_URL + 'lasana/css/%s.css' % (style or 'default')
+
+    return {
+        'lasana_name': LASANA_NAME,
+        'stylesheet' : stylesheet,
+        }
