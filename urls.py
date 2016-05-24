37e6e30cf0edd9a4579d7a3a2918865ca1be86f0
Juan Luis Boya García <ntrrgc@gmail.com>
2016-05-24 03:30:46 +0200

    Allow lowercase in urls.py too

--- urls.py
+++ urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^$', views.MealCreateView.as_view(), name='meal-create'),
-    url(r'^(?P<meal_id>[A-Z0-9]+)/?$', views.MealServeView.as_view(), name='meal-serve'),
+    url(r'^(?P<meal_id>[a-zA-Z0-9]+)/?$', views.MealServeView.as_view(), name='meal-serve'),
     url(r'^set_style/$', views.SetStyleView.as_view(), name='set-style'),
 
     url(r'^api/v1/$', views.MealCreateAPIView.as_view()),
